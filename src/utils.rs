6f055cddfd083327baa5102c3194929ca0563a0a
Beskhue <thomas@kepow.org>
2018-12-03 11:22:14 +0100

    Simplify namespacing.

--- src/utils.rs
+++ src/utils.rs
@@ -2,11 +2,11 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-pub fn lines_from_file<P>(filename: P) -> Result<Vec<String>, io::Error>
+pub fn lines_from_file<P>(filename: P) -> io::Result<Vec<String>>
 where
     P: AsRef<Path>,
 {
     let file = try!(File::open(filename));
-    let buf = std::io::BufReader::new(file);
+    let buf = io::BufReader::new(file);
     buf.lines().collect()
 }
