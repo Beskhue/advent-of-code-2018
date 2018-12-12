e3c2dae05be657caf64aa76da94ce2cc90569578
Beskhue <thomas@kepow.org>
2018-12-12 08:36:48 +0100

    Add rustfmt configuration and apply to utils.

--- src/utils.rs
+++ src/utils.rs
@@ -6,7 +6,7 @@ pub fn lines_from_file<P>(filename: P) -> io::Result<Vec<String>>
 where
     P: AsRef<Path>,
 {
-    let file = try!(File::open(filename));
+    let file = File::open(filename)?;
     let buf = io::BufReader::new(file);
     buf.lines().collect()
 }
